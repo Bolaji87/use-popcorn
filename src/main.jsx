ba358044be3568a82c9fac1df15be898b4e107e1
Bolaji87 <sakabolaji77@gmail.com>
2024-07-17 21:19:00 +0100

    reset app and main components

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,28 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import App from "./App.jsx";
-// import "./index.css";
+import App from "./App.jsx";
+import "./index.css";
 import StarRating from "./components/StarRating.jsx";
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
 
-  return (
-    <div>
-      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
-      <p>This movie is rated {movieRating} stars</p>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+//       <p>This movie is rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={["terrible", "bad", "okay", "good", "amazing"]}
     />
     <StarRating maxRating={5} defaultRating={3} color="red" size="25" />
-    <Test />
+    <Test /> */}
   </React.StrictMode>
 );
